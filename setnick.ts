dd078e21caa934ef234ecbd3ccc2fc1173efd9de
LPB <76528559+LPB8479@users.noreply.github.com>
2021-11-06 22:04:31 -0400

    Create setnick.ts

---
+++ setnick.ts
@@ -0,0 +1,25 @@
+import { config } from '../config/config';
+
+config.commands.on(
+  {
+    name: 'setnick',
+    description: "Changes a member's Nickname",
+    filters: discord.command.filters.canManageNicknames()
+  },
+  (args) => ({
+    member: args.guildMember(),
+    input: args.string()
+  }),
+  async (message, { member, input }) => {
+    try {
+      await member.edit({
+        nick: input
+      });
+      await message.reply(
+        `${member.user.getTag()}'s nickname has been set to ${input}`
+      );
+    } catch (err) {
+      await message.reply(`My permissions are not enough.`);
+    }
+  }
+);
