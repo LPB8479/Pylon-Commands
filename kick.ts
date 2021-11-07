0a4a2e3449732f71cf8599170f94b920d95846b5
LPB <76528559+LPB8479@users.noreply.github.com>
2021-11-06 22:03:48 -0400

    Create kick.ts

---
+++ kick.ts
@@ -0,0 +1,45 @@
+import { config } from '../config/config';
+
+config.commands.on(
+  {
+    name: 'kick',
+    description: 'Kick another user',
+    filters: discord.command.filters.canKickMembers()
+  },
+
+  (args) => ({
+    member: args.guildMember(),
+    reason: args.textOptional()
+  }),
+  async (message, { member, reason }) => {
+    async function UsesCounter(key: any, by: number = 1): Promise<number> {
+      const nextValue = await config.kv.modlog.transact<number>(
+        key,
+        (prevValue = 0) => {
+          return prevValue + by;
+        }
+      );
+      return nextValue!;
+    }
+    await message.reply('Kicked **' + member.user.getTag() + '**.');
+    await member.kick();
+    const channel = await discord.getGuildTextChannel(config.channel.modlog);
+    let uses = await UsesCounter('count');
+    // Assemble embed
+    var embed = new discord.Embed();
+    embed.setTitle('kick | case ' + uses);
+    embed.setColor(0x4c88ff);
+    embed.setDescription(
+      '**Offender:** ' +
+        member.user.getTag() +
+        member.user.toMention() +
+        '\n**Reason:** ' +
+        reason +
+        '\n**Responsible moderator:** ' +
+        message.author.getTag()
+    );
+    embed.setFooter({ text: 'ID: ' + member.user.id });
+    embed.setTimestamp(new Date().toISOString());
+    await channel.sendMessage(embed);
+  }
+);
