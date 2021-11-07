ee840ffca1917523be6d61e17c6dd55edf527bec
LPB <76528559+LPB8479@users.noreply.github.com>
2021-11-06 21:52:08 -0400

    Create addemoji.ts

---
+++ addemoji.ts
@@ -0,0 +1,55 @@
+async function urlToArrayBuffer(url: string) {
+  const emoji = await (await fetch(url)).arrayBuffer();
+  return emoji;
+}
+
+new discord.command.CommandGroup({
+    defaultPrefix: '&',
+    additionalPrefixes: ['!'],
+    mentionPrefix: true
+  }).subcommand(
+  {
+    name: 'addemoji',
+    description: 'Add server emoji',
+    filters: discord.command.filters.canManageEmojis()
+  },
+  (subcommand) => {
+    subcommand.on(
+      'color',
+      (args) => ({
+        emojiName: args.string(),
+        hex: args.string()
+      }),
+      async (message, { emojiName, hex }) => {
+        const color = hex.replace('#', '').toLowerCase;
+        const emoji = urlToArrayBuffer(
+          'https://res.cloudinary.com/demo/w_150,h_150/c_fill,r_max/e_colorize,co_rgb:' +
+            color +
+            '/one_pixel.png'
+        );
+        const guild = await message.getGuild();
+        await guild.createEmoji({
+          image: emoji,
+          name: emojiName
+        });
+        message.reply('Success!');
+      }
+    );
+
+    subcommand.default(
+      (args) => ({
+        emojiName: args.string(),
+        emojiUrl: args.string()
+      }),
+      async (message, { emojiName, emojiUrl }) => {
+        const emoji = urlToArrayBuffer(emojiUrl);
+        const guild = await message.getGuild();
+        await guild.createEmoji({
+          image: emoji,
+          name: emojiName
+        });
+        message.reply('Success!');
+      }
+    );
+  }
+);
