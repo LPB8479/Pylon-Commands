a56313ae6bf234e0493e52f8cd6a47115a8b5ffb
LPB <76528559+LPB8479@users.noreply.github.com>
2021-11-08 13:55:16 -0500

    Update ban.ts

--- ban.ts
+++ ban.ts
@@ -21,15 +21,17 @@ config.commands.on(
       );
       return nextValue!;
     }
-    await message.reply('Banned **' + member.user.getTag() + '**.');
-    await member.ban();
+    await message.reply(`Banned **${member.user.getTag()}**.`);
+    //await member.kick();
     const channel = await discord.getGuildTextChannel('875212427132805180');
     let uses = await UsesCounter('count');
     // Assemble embed
     var embed = new discord.Embed();
-    embed.setTitle('ban | Case ' + uses);
+    embed.setTitle(`ban | Case ${uses}`);
     embed.setColor(0xdd5e53);
-    embed.setDescription(`**Offender:** ${member.user.getTag()} ${member.user.toMention()}\n**Reason:**' ${reason}'\n**Responsible moderator:**${message.author.getTag()}`);
+    embed.setDescription(
+      `**Offender:** ${member.user.getTag()} ${member.user.toMention()}\n**Reason:**' ${reason}'\n**Responsible moderator:**${message.author.getTag()}`
+    );
     embed.setFooter({ text: `ID: ${member.user.id}` });
     embed.setTimestamp(new Date().toISOString());
     await channel.sendMessage(embed);
