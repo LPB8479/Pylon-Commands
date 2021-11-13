d5d35dc6df55619bc3fc1cefacf8e30b0371c1d6
LPB <76528559+LPB8479@users.noreply.github.com>
2021-11-13 15:33:27 -0500

    Create clonechannel.ts

---
+++ clonechannel.ts
@@ -0,0 +1,23 @@
+import { config } from '../config/config';
+
+config.commands.on(
+  {
+    filters: discord.command.filters.canManageChannels(),
+    name: 'clonechannel',
+    description: 'Create a channel with permissions copied from another channel'
+  },
+  (args) => ({
+    baseChannel: args.guildChannel(),
+    channelName: args.text()
+  }),
+  async (message, { baseChannel, channelName }) => {
+    const perms = baseChannel.permissionOverwrites;
+    const parent = baseChannel.parentId;
+    const server = await message.getGuild();
+    await server.createChannel({
+      name: channelName,
+      permissionOverwrites: perms,
+      parentId: parent
+    });
+  }
+);
