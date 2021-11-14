94a92a5900f45cfa94e9dd5af1840c45b198a42e
LPB <76528559+LPB8479@users.noreply.github.com>
2021-11-13 22:44:54 -0500

    Create echo.ts

---
+++ echo.ts
@@ -0,0 +1,29 @@
+import { config } from '../config/config';
+
+// Example usage: `!echo foobar` OR `!repeat foobar`
+config.commands.on(
+  {
+    name: 'echo',
+    aliases: ['repeat'],
+    filters: discord.command.filters.canManageMessages(),
+    description: 'Get the bot to repeat whatever you say'
+  },
+  (args) => ({
+    input: args.text()
+  }),
+  async (message, { input }) => {
+    // Send a message
+    const messageArray = input
+      .replace('<#', '')
+      .replace('>', '')
+      .split(' ');
+    const destChannel =
+      input.includes('#') == true
+        ? await discord.getGuildTextChannel(messageArray[0].toString())
+        : await message.getChannel();
+    await destChannel?.sendMessage({
+      content: `${input.includes('#') ? messageArray.slice(1) : input}`,
+      allowedMentions: {} // Don't allow this message to ping anyone
+    });
+  }
+);
