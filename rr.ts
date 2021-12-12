af248a493a13400da0ceb56185d8adc0207e2f61
Albert Portnoy <albert@albertp.dev>
2021-12-11 20:07:35 -0600

    Fixes

--- rr.ts
+++ rr.ts
@@ -7,13 +7,6 @@
 */
 const config: Config = {};
 
-/*
-NORMAL: add role on reaction add, remove role on reaction remove
-VERIFY: add role and remove reaction on reaction add. Do not allow role removal.
-REVERSE: remove role on reaction add, add role on reaction remove
-REVERSE_VERIFY: remove role and reaction on reaction remove. Do not allow role addition.
-TOGGLE: toggle role and remove reaction on reaction add.
- */
 interface ReactionRoleConfig {
 	role: string;
 	mode: 'NORMAL' | 'VERIFY' | 'REVERSE' | 'REVERSE_VERIFY' | 'TOGGLE';
@@ -44,17 +37,28 @@ discord.on(discord.Event.MESSAGE_REACTION_ADD, async reaction => {
 	let config = getReactionConfig(reaction);
 	if (!config) return;
 
-	if (['NORMAL', 'VERIFY'].includes(config.mode) || (['TOGGLE'].includes(config.mode) && !reaction.member!.hasRole(config.role))) {
+	if (
+		['NORMAL', 'VERIFY'].includes(config.mode) ||
+		(['TOGGLE'].includes(config.mode) &&
+			!reaction.member!.roles.includes(config.role))
+	) {
 		await reaction.member!.addRole(config.role);
-	if (['REVERSE', 'REVERSE_VERIFY'].includes(config.mode) || (['TOGGLE'].includes(config.mode) && reaction.member!.hasRole(config.role)))
+	}
+	if (
+		['REVERSE', 'REVERSE_VERIFY'].includes(config.mode) ||
+		(['TOGGLE'].includes(config.mode) &&
+			reaction.member!.roles.includes(config.role))
+	) {
 		await reaction.member!.removeRole(config.role);
+	}
 	const channel = (await discord.getGuildTextChannel(reaction.channelId))!;
 	const message = (await channel.getMessage(reaction.messageId))!;
-	if (['VERIFY', 'REVERSE_VERIFY'].includes(config.mode))
+	if (['VERIFY', 'REVERSE_VERIFY'].includes(config.mode)) {
 		message.deleteReaction(
 			emojiIdentifier,
 			reaction.member!.user.id.toString(),
 		);
+	}
 });
 
 discord.on(discord.Event.MESSAGE_REACTION_REMOVE, async reaction => {
@@ -63,10 +67,12 @@ discord.on(discord.Event.MESSAGE_REACTION_REMOVE, async reaction => {
 	let config = getReactionConfig(reaction);
 	if (!config) return;
 
-	if (['REVERSE'].includes(config.mode))
+	if (['REVERSE'].includes(config.mode)) {
 		await reaction.member!.addRole(config.role);
-	if (['NORMAL'].includes(config.mode))
+	}
+	if (['NORMAL'].includes(config.mode)) {
 		await reaction.member!.removeRole(config.role);
+	}
 
 	const channel = (await discord.getGuildTextChannel(reaction.channelId))!;
 	const message = (await channel.getMessage(reaction.messageId))!;
