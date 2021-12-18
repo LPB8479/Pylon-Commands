62f8521f0cc12dc4069418b9bda471a6457f9272
LBP8479 <littleplasticbrick@gmail.com>
2021-12-18 16:00:51 -0500

    Create embedsource.ts

---
+++ embedsource.ts
@@ -0,0 +1,20 @@
+import { config } from '../config/config';
+//Usage: [p]embedsource <message id> [channel]
+config.commands.on(
+    {
+        name: 'embedsource',
+        description: 'Get the raw json from any embed'
+    },
+    (args) => ({
+        messageID: args.string(),
+        channel: args.guildTextChannelOptional()
+    }),
+    async (message, { messageID, channel }) => {
+        var basechannel = channel == null ? await message.getChannel() : await discord.getGuildTextChannel(channel)
+        var targetMessage = await basechannel.getMessage(messageID);
+        const embeds = targetMessage?.embeds.map((e) => {
+            return e.serialize();
+          });
+          await message.reply('```' + JSON.stringify(embeds, null, 2) + '```');
+    }
+);
