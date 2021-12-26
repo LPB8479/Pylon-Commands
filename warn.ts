78cd39146729ad47b6fc2169129b2b2c2cb3eca5
LBP8479 <littleplasticbrick@gmail.com>
2021-12-25 20:09:50 -0500

    Create warn.ts

---
+++ warn.ts
@@ -0,0 +1,39 @@
+import { config } from '../config';
+//Usage: [p]warn <member> [reason]
+config.commands.on(
+  {
+    name: 'warn',
+    description: 'Warn another user',
+    filters: discord.command.filters.canManageRoles()
+    //  filters: discord.command.filters.hasRole(config.role.moderator)
+  },
+
+  (args) => ({
+    member: args.guildMember(),
+    reason: args.textOptional()
+  }),
+  async (message, { member, reason }) => {
+    async function UsesCounter(key: any, by: number = 1): Promise<number> {
+      const nextValue = await config.kv.modlog.transact<number>(
+        key,
+        (prevValue = 0) => {
+          return prevValue + by;
+        }
+      );
+      return nextValue!;
+    }
+    await message.reply(`**${member.user.getTag()}** has been warned.\n**Reason:** ${reason}`);
+    const channel = await discord.getGuildTextChannel(config.channel.modlog);
+    let uses = await UsesCounter('count');
+    // Assemble embed
+    var embed = new discord.Embed();
+    embed.setTitle(`Warn | Case ${uses}`);
+    embed.setColor(0xfcf77a);
+    embed.setDescription(
+      `**Offender:** ${member.user.getTag()} ${member.user.toMention()}\n**Reason:** ${reason}\n**Responsible moderator:**${message.author.getTag()}`
+    );
+    embed.setFooter({ text: `ID: ${member.user.id}` });
+    embed.setTimestamp(new Date().toISOString());
+    await channel.sendMessage(embed);
+  }
+);
