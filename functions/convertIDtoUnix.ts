4c577db128eb96b4d32ce3995df8d8b7ef431d1f
LBP8479 <littleplasticbrick@gmail.com>
2021-12-31 14:17:27 -0500

    Update convertIDtoUnix.ts

--- functions/convertIDtoUnix.ts
+++ functions/convertIDtoUnix.ts
@@ -1,6 +1,6 @@
-export function convertIDtoUnix(snowflake: number) {
+export function convertIDtoUnix(snowflake: string) {
   // snowflake to date object
-  const date = new Date(Math.floor(snowflake / 4194304) + 1420070400000);
+  const date = new Date(Math.floor(parseInt(snowflake) / 4194304) + 1420070400000);
   // date object to unix
   const unix = parseInt((new Date(date).getTime() / 1000).toFixed(0));
   return unix;
