359651da963eece146449a3e58c8c75bb47eeb83
LBP8479 <littleplasticbrick@gmail.com>
2023-06-23 08:09:27 -0400

    Update currency.ts

--- utility/currency.ts
+++ utility/currency.ts
@@ -11,8 +11,27 @@ config.commands.on(
         amtIn: args.numberOptional()
     }),
     async (message, { from, to, amtIn }) => {
-        var req = await fetch(`https://api.exchangerate.host/convert?from=${from}&to=${to}`);
-        var data = await req.json();
-        var rate = data.result;
+        var fromCap = from.toUpperCase();
+        var toCap = to.toUpperCase();
+
+        var rateReq = await fetch(`https://api.exchangerate.host/convert?from=${fromCap}&to=${toCap}`);
+        var rateData = await rateReq.json();
+        var rate: number = rateData.result;
+
+        var symbReq = await fetch(`https://gist.githubusercontent.com/ksafranski/2973986/raw/5fda5e87189b066e11c1bf80bbfbecb556cf2cc1/Common-Currency.json`);
+        var symbData = await symbReq.json();
+        var symbFrom = symbData[fromCap].symbol_native;
+        var symbTo = symbData[toCap].symbol_native;
+
+        var amtFrom = (amtIn == null) ? 1 : amtIn;
+        var amtTo = amtFrom * rate;
+
+        // make embed
+        var embed = new discord.Embed()
+            .setTitle(`Currency Conversion: **${fromCap}** to **${toCap}**`)
+            .setColor(0x9cff1d)
+            .setDescription(`The conversion rate between \`${fromCap}\` and \`${toCap}\` is \`${rate}\`.\n\n\`${symbFrom}${amtFrom} ${fromCap}\` is equal to \`${symbTo}${amtTo} ${toCap}\`.`);
+        // send embed
+        await message.reply({ content: '', embed: embed });
     }
 );
